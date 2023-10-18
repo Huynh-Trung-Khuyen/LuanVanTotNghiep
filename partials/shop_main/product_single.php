5cdfa8ebc2431192cf9a0316d50e1fab8f1aaea8
KhuyenB1910534 <khuyenb1910534@student.ctu.edu.vn>
2023-10-18 12:31:41 +0700

    giới hạn số lượng mua không vượt quá số lượng có

--- partials/shop_main/product_single.php
+++ partials/shop_main/product_single.php
@@ -1,42 +1,50 @@
 <?php
-// Bắt đầu hoặc tiếp tục phiên
-if (isset($_GET['id']) && isset($_POST['add_to_cart'])) {
-    $product_id = $_GET['id']; 
-    $quantity_of_products = $_POST['quantity']; 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $product_id = $_GET['id'];
+    $quantity_of_products = $_POST['quantity'];
 
     if (isset($_SESSION['user_id'])) {
         $user_id = $_SESSION['user_id'];
 
-        // Kiểm tra xem sản phẩm đã tồn tại trong giỏ hàng chưa
-        $query = "SELECT * FROM cart WHERE product_id = :product_id AND user_id = :user_id";
-        $stmt = $conn->prepare($query);
-        $stmt->bindParam(':product_id', $product_id);
-        $stmt->bindParam(':user_id', $user_id);
-        $stmt->execute();
-        $existingCartItem = $stmt->fetch(PDO::FETCH_ASSOC);
+        $query = $conn->prepare('SELECT w.quantity FROM warehouse w JOIN product p ON w.warehouse_id = p.warehouse_id WHERE p.product_id = :product_id');
+        $query->bindParam(':product_id', $product_id);
+        $query->execute();
+        $product_quantity = $query->fetch(PDO::FETCH_ASSOC);
 
-        if ($existingCartItem) {
-            // Nếu sản phẩm đã tồn tại trong giỏ hàng, cập nhật số lượng
-            $newQuantity = $existingCartItem['quantity_of_products'] + $quantity_of_products;
-            $updateQuery = "UPDATE cart SET quantity_of_products = :quantity WHERE cart_id = :cart_id";
-            $updateStmt = $conn->prepare($updateQuery);
-            $updateStmt->bindParam(':quantity', $newQuantity);
-            $updateStmt->bindParam(':cart_id', $existingCartItem['cart_id']);
-            $updateStmt->execute();
+        if ($product_quantity) {
+            if ($quantity_of_products < 1) {
+                echo "Vui lòng chọn số lượng hợp lệ.";
+            } elseif ($quantity_of_products > $product_quantity['quantity']) {
+                echo "Số lượng sản phẩm vượt quá số lượng hiện có.";
+            } else {
+                $query = "SELECT * FROM cart WHERE product_id = :product_id AND user_id = :user_id";
+                $stmt = $conn->prepare($query);
+                $stmt->bindParam(':product_id', $product_id);
+                $stmt->bindParam(':user_id', $user_id);
+                $stmt->execute();
+                $existingCartItem = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                if ($existingCartItem) {
+                    $newQuantity = $existingCartItem['quantity_of_products'] + $quantity_of_products;
+                    $updateQuery = "UPDATE cart SET quantity_of_products = :quantity WHERE cart_id = :cart_id";
+                    $updateStmt = $conn->prepare($updateQuery);
+                    $updateStmt->bindParam(':quantity', $newQuantity);
+                    $updateStmt->bindParam(':cart_id', $existingCartItem['cart_id']);
+                    $updateStmt->execute();
+                } else {
+                    $insertQuery = "INSERT INTO cart (user_id, product_id, quantity_of_products) VALUES (:user_id, :product_id, :quantity)";
+                    $insertStmt = $conn->prepare($insertQuery);
+                    $insertStmt->bindParam(':user_id', $user_id);
+                    $insertStmt->bindParam(':product_id', $product_id);
+                    $insertStmt->bindParam(':quantity', $quantity_of_products);
+                    $insertStmt->execute();
+                }
+                echo "Sản phẩm đã được thêm vào giỏ hàng.";
+            }
         } else {
-            // Nếu sản phẩm chưa tồn tại trong giỏ hàng, thêm vào
-            $insertQuery = "INSERT INTO cart (user_id, product_id, quantity_of_products) VALUES (:user_id, :product_id, :quantity)";
-            $insertStmt = $conn->prepare($insertQuery);
-            $insertStmt->bindParam(':user_id', $user_id);
-            $insertStmt->bindParam(':product_id', $product_id);
-            $insertStmt->bindParam(':quantity', $quantity_of_products);
-            $insertStmt->execute();
+            echo "Không có sản phẩm hoặc đã hết hàng.";
         }
-
-        // Hiển thị thông báo sản phẩm đã được thêm vào giỏ hàng
-        echo "Sản phẩm đã được thêm vào giỏ hàng.";
     } else {
-        // Xử lý trường hợp 'user_id' không tồn tại trong phiên
         echo "Vui lòng đăng nhập trước khi thêm sản phẩm vào giỏ hàng.";
     }
 }
@@ -65,7 +73,7 @@ if (isset($_GET['id'])) {
                     <h3><?php echo $row['product_name'] ?></h3>
                     <p class="price"><span></strong><?php echo $row['price'] ?>.000 vnđ/Kg</span></p>
                     <p><?php echo $row['content'] ?></p>
-                    <p>Số lượng hiện có: <?php echo $product_quantity['quantity'] ?></p>
+                    <p>Số lượng hiện có: <?php echo $product_quantity['quantity'] ?>Kg</p>
                     <div class="row mt-4">
                         <div class="w-100"></div>
                         <div class="input-group col-md-6 d-flex mb-3">
@@ -83,7 +91,7 @@ if (isset($_GET['id'])) {
                         </div>
                         <div class="w-100"></div>
                     </div>
-                    <button type="submit" class="btn" name="add_to_cart">Add to Cart</button>
+                    <button type="submit" class="btn" name="add_to_cart">Thêm Vào Giỏ Hàng</button>
                 </div>
             </div>
         </div>
