a03f5ae4d1ac45aa3ab13ff61603880b35d34f1f
KhuyenB1910534 <khuyenb1910534@student.ctu.edu.vn>
2023-10-30 15:20:47 +0700

    khi xóa kho thì xóa luôn phiên đấu giá

--- public/admin/warehouse_bid/delete.php
+++ public/admin/warehouse_bid/delete.php
@@ -6,6 +6,11 @@ require_once '../../../config.php';
 if (isset($_GET['id'])) {
     $warehouse_bid_id = $_GET['id'];
 
+    $deleteProductBidQuery = $conn->prepare('DELETE FROM product_bid WHERE warehouse_bid_id = :warehouse_bid_id');
+    $deleteProductBidQuery->bindParam(':warehouse_bid_id', $warehouse_bid_id);
+    $deleteProductBidQuery->execute();
+
+ 
     $deleteWarehouseQuery = $conn->prepare('DELETE FROM warehouse_bid WHERE warehouse_bid_id = :warehouse_bid_id');
     $deleteWarehouseQuery->bindParam(':warehouse_bid_id', $warehouse_bid_id);
     $deleteWarehouseQuery->execute();
