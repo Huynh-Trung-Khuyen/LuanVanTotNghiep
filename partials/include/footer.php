c547179f017052f7b9add8e123f1737851f72f0a
KhuyenB1910534 <khuyenb1910534@student.ctu.edu.vn>
2023-11-06 15:04:18 +0700

    sửa footer

--- partials/include/footer.php
+++ partials/include/footer.php
@@ -1,77 +1,68 @@
 <footer class="ftco-footer ftco-section">
-      <div class="container">
-      	
-        <div class="row mb-5">
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Vegefoods</h2>
-              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
-              <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4 ml-md-5">
-              <h2 class="ftco-heading-2">Menu</h2>
-              <ul class="list-unstyled">
-                <li><a href="#" class="py-2 d-block">Shop</a></li>
-                <li><a href="#" class="py-2 d-block">About</a></li>
-                <li><a href="#" class="py-2 d-block">Journal</a></li>
-                <li><a href="#" class="py-2 d-block">Contact Us</a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-md-4">
-             <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Help</h2>
-              <div class="d-flex">
-	              <ul class="list-unstyled mr-l-5 pr-l-3 mr-4">
-	                <li><a href="#" class="py-2 d-block">Shipping Information</a></li>
-	                <li><a href="#" class="py-2 d-block">Returns &amp; Exchange</a></li>
-	                <li><a href="#" class="py-2 d-block">Terms &amp; Conditions</a></li>
-	                <li><a href="#" class="py-2 d-block">Privacy Policy</a></li>
-	              </ul>
-	              <ul class="list-unstyled">
-	                <li><a href="#" class="py-2 d-block">FAQs</a></li>
-	                <li><a href="#" class="py-2 d-block">Contact</a></li>
-	              </ul>
-	            </div>
-            </div>
-          </div>
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4">
-            	<h2 class="ftco-heading-2">Have a Questions?</h2>
-            	<div class="block-23 mb-3">
-	              <ul>
-	                <li><span class="icon icon-map-marker"></span><span class="text"></span></li>
-	                <li><a href="#"><span class="icon icon-phone"></span><span class="text"></span></a></li>
-	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text"></span></a></li>
-	              </ul>
-	            </div>
-            </div>
+  <div class="container">
+    <div class="row mb-5">
+      <div class="col-md">
+        <div class="ftco-footer-widget mb-4">
+          <h2 class="ftco-heading-2"><i class="fa-solid fa-seedling"></i>FARMBIDS</h2>
+          <p>Trang web thương mại điện tử cửa hàng nông sản sạch kết hợp với hệ thống đấu giá trực tuyến dành cho các doanh nghiệp</p>
+          <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+            <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+            <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+            <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md">
+      <div class="ftco-footer-widget mb-4 ml-md-5">
+          <h2 class="ftco-heading-2">Menu</h2>
+          <ul class="list-unstyled">
+            <li><a href="#" class="py-2 d-block">Shop</a></li>
+            <li><a href="#" class="py-2 d-block">About</a></li>
+            <li><a href="#" class="py-2 d-block">Contact Us</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="ftco-footer-widget mb-4 ml-md-5">
+          <h2 class="ftco-heading-2">Trợ Giúp</h2>
+            <ul class="list-unstyled">
+              <li><a href="#" class="py-2 d-block">Thông Tin Giao Hàng</a></li>
+              <li><a href="#" class="py-2 d-block">Hủy Đơn &amp; Hoàn Trả</a></li>
+              <li><a href="#" class="py-2 d-block">Thông Tin Cá Nhân</a></li>
+            </ul>     
+        </div>
+      </div>
+      <div class="col-md">
+        <div class="ftco-footer-widget mb-4">
+          <h2 class="ftco-heading-2">Liên Hệ</h2>
+          <div class="block-23 mb-3">
+            <ul>
+
+            <li><a href="#"><i class="fa-solid fa-phone"></i><br>SĐT</a></li>
+              <li><a href="#"><i class="fa-solid fa-envelope"></i>khuyenb1910534@student.ctu.edu.vn</a></li>
+            </ul>
           </div>
         </div>
-        
       </div>
-    </footer>
+    </div>
+
+  </div>
+</footer>
 
 
 <script src="../../css/js/jquery.min.js"></script>
-  <script src="../../css/js/jquery-migrate-3.0.1.min.js"></script>
-  <script src="../../css/js/popper.min.js"></script>
-  <script src="../../css/js/bootstrap.min.js"></script>
-  <script src="../../css/js/jquery.easing.1.3.js"></script>
-  <script src="../../css/js/jquery.waypoints.min.js"></script>
-  <script src="../../css/js/jquery.stellar.min.js"></script>
-  <script src="../../css/js/owl.carousel.min.js"></script>
-  <script src="../../css/js/jquery.magnific-popup.min.js"></script>
-  <script src="../../css/js/aos.js"></script>
-  <script src="../../css/js/jquery.animateNumber.min.js"></script>
-  <script src="../../css/js/bootstrap-datepicker.js"></script>
-  <script src="../../css/js/scrollax.min.js"></script>
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-  <script src="../../css/js/google-map.js"></script>
-  <script src="../../css/js/main.js"></script>
+<script src="../../css/js/jquery-migrate-3.0.1.min.js"></script>
+<script src="../../css/js/popper.min.js"></script>
+<script src="../../css/js/bootstrap.min.js"></script>
+<script src="../../css/js/jquery.easing.1.3.js"></script>
+<script src="../../css/js/jquery.waypoints.min.js"></script>
+<script src="../../css/js/jquery.stellar.min.js"></script>
+<script src="../../css/js/owl.carousel.min.js"></script>
+<script src="../../css/js/jquery.magnific-popup.min.js"></script>
+<script src="../../css/js/aos.js"></script>
+<script src="../../css/js/jquery.animateNumber.min.js"></script>
+<script src="../../css/js/bootstrap-datepicker.js"></script>
+<script src="../../css/js/scrollax.min.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+<script src="../../css/js/google-map.js"></script>
+<script src="../../css/js/main.js"></script>
