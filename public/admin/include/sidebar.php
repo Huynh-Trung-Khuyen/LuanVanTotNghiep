d23343cb06e4c646fa59201a11c65eb708e99448
KhuyenB1910534 <khuyenb1910534@student.ctu.edu.vn>
2023-11-14 18:29:07 +0700

    Đổi ảnh admin

--- public/admin/include/sidebar.php
+++ public/admin/include/sidebar.php
@@ -23,7 +23,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="../../template/admin/dist/img/admin.png" class="img-circle elevation-2" alt="User Image">
+                <img src="../../template/admin/dist/img/admin.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a href="../main_page/index.php" class="d-block">
